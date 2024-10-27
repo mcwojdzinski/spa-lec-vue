c4265cc0f6991e810d9b4e195b431b0b790903fd
Mateusz Cwojdziński <mateuszcwojdzinski@outlook.com>
2024-10-27 16:04:23 +0100

    Add Finished tasks 1 and 2

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import TableOfPeople from "@/components/TableOfPeopleCompositionAPI.vue";
+import TableOfPeopleOptionsAPI from "@/components/TableOfPeopleOptionsAPI.vue";
+</script>
+
+<template>
+  <div>
+
+    <h1>Component with <b>Composition API</b></h1>
+    <TableOfPeople />
+  </div>
+  <div>
+    <h1>Component with <b>Option API</b></h1>
+    <TableOfPeopleOptionsAPI />
+  </div>
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/TableOfPeopleOptionsAPI.vue
@@ -0,0 +1,87 @@
+<script>
+export default {
+  data() {
+    return {
+      firstName: '',
+      lastName: '',
+      peopleArr: [],
+    }
+  },
+  computed: {
+    numberOfPeople() {
+      return this.peopleArr.length
+    }
+  },
+  methods: {
+    addPerson() {
+      this.peopleArr.push({firstName: this.firstName, lastName: this.lastName});
+      this.firstName.value = '';
+      this.lastName.value = '';
+    }
+  }
+}
+</script>
+
+
+<template>
+  <div class="table-wrapper">
+
+    <form @submit.prevent="addPerson" class="form">
+      <label for="firstName">
+        First name:
+        <input v-model="firstName" placeholder="Enter first name" type="text" id="firstName"/>
+      </label>
+      <label for="lastname">
+        Last name:
+        <input v-model="lastName" placeholder="Enter last name" type="text" id="lastName"/>
+      </label>
+      <button type="submit">Add person</button>
+    </form>
+
+    <table class="table">
+      <thead>
+      <td>Nr.</td>
+      <td>Imię</td>
+      <td>Nazwisko</td>
+      </thead>
+      <tbody>
+      <tr v-for="(person, index) in peopleArr" :key="index">
+        <td>{{ index }}</td>
+        <td>{{ person.firstName }}</td>
+        <td>{{ person.lastName}}</td>
+      </tr>
+      </tbody>
+    </table>
+  </div>
+  <p class="total">Total people count: {{ numberOfPeople }}</p>
+</template>
+
+<style scoped>
+.table-wrapper {
+  display: flex;
+  flex-direction: row;
+  column-gap: 30px;
+  align-items: flex-start;
+}
+.form {
+  display: flex;
+  flex-direction: column;
+  row-gap: 15px;
+
+}
+.table {
+  width: 100%;
+  border-collapse: collapse;
+}
+.table td {
+  border: 1px solid brown;
+}
+
+.table thead {
+  background-color: chocolate;
+}
+
+.total {
+  text-align: right;
+}
+</style>
